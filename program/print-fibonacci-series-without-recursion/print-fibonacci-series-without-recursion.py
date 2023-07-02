ec21b102f7e50efc4651ac0a65d70d10303fe93d
Aashray24092000 <118126127+Aashray24092000@users.noreply.github.com>
2023-07-02 12:07:55 +0530

    Update print-fibonacci-series-without-recursion.py

--- program/print-fibonacci-series-without-recursion/print-fibonacci-series-without-recursion.py
+++ program/print-fibonacci-series-without-recursion/print-fibonacci-series-without-recursion.py
@@ -2,8 +2,7 @@ terms = int(input("Enter the number of terms:"))
 f, s = 0, 1
 print("Fibonacci series are:")
 print(f, s, end=" ")
-
-while terms - 2:
+for i in range(2,terms):
     t = f + s
     f = s
     s = t
